8cd1fda18691abd334f57c3b140de63384edd5b3
Sashakt <saskhatenterprises@gmail.com>
2026-07-24 15:34:52 +0530

    update the code

--- app.ts
+++ app.ts
@@ -14,7 +14,7 @@ const app: Express = express();
 
 // CORS — allow frontend origin
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://employee-management-system-theta-cyan.vercel.app'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://employee-management-system-ritik.vercel.app/'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
